90a5b031b0896c72ff9c610957c4f70c31e6b12a
zaorsha <zaorsha@gmail.com>
2025-11-24 22:04:02 +0000

    more plotly express

--- die_visual.py
+++ die_visual.py
@@ -5,19 +5,15 @@ die = Die()
 die2 = Die()
 
 results = []
-count = [0,0,0,0,0,0]
+count = [0,0,0,0,0,0,0,0,0,0,0,0]
 
-for i in range(10000):
-    roll_1 = die.roll()
-    roll_2 = die2.roll()
-    results.append((roll_1, roll_2))
-    count[roll_1 -1] += 1
-    count[roll_2 - 1] += 1
-
-print(f'1: {count[0]}\n2: {count[1]}\n3: {count[2]}\n4: {count[3]}\n5: {count[4]}\n6: {count[5]}')
+for i in range(100000):
+    total = die.roll() + die2.roll()
+    results.append(total)
+    count[total -1] += 1
 
 # Visualise the results
 title = 'Roll of a dice'
 labels = {'x': 'Result', 'y': 'Frequency of Result'}
-fig = px.bar(x=range(1,7), y=count, title=title, labels=labels)
+fig = px.bar(x=range(1,13), y=count, title=title, labels=labels)
 fig.show()
