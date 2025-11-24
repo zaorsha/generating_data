044536b4f2ff4de3cad4cabe92c8879e54340016
zaorsha <zaorsha@gmail.com>
2025-11-24 22:10:50 +0000

    adding TODO tags

--- die_visual.py
+++ die_visual.py
@@ -5,6 +5,8 @@ die = Die()
 die2 = Die()
 
 results = []
+
+# FIXME - count should grow dynamically based on Dice size
 count = [0,0,0,0,0,0,0,0,0,0,0,0]
 
 for i in range(100000):
@@ -15,6 +17,8 @@ for i in range(100000):
 # Visualise the results
 title = 'Roll of a dice'
 labels = {'x': 'Result', 'y': 'Frequency of Result'}
+
+# TODO - Once count is fixed, change range below so it only shows results with data
 fig = px.bar(x=range(1,13), y=count, title=title, labels=labels)
 
 fig.update_layout(xaxis_dtick=1)
